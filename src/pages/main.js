4ddea5f321c5744136218f7e6bb0e7794687e50d
always0ne <si8363@naver.com>
2020-11-08 15:10:39 +0900

    docs: 앞으로 진행할 방향 주석에 적음

--- src/pages/main.js
+++ src/pages/main.js
@@ -10,6 +10,8 @@ import './main.css'
 const Main = ({type}) => {
     const [checklist, setChecklist] = useState([]);
     const [searchString, setSearchString] = useState("");
+    const [usable, setUsable] = useState([]);
+    const [recommends, setRecommends] = useState("");
 
     const getVersions = (selectedValue) => {
         setChecklist(selectedValue);
@@ -18,17 +20,24 @@ const Main = ({type}) => {
 
     const searchReservations = (string) => {
         setSearchString(string);
-        console.log(searchString)
+        console.log(searchString);
+        // 검색로직 여기에 하거나 따로 파일 빼도 됨
+        // 검색 (json 파일 열람 하여 스캔)
+        // 통과한 checklist 추가
+        setUsable("");
+        // 검색 후 불가한 DB가 생기면 동의어 조회해서 여기에 넘겨주면 됨
+        setRecommends("")
     };
-    
+
+
     return (
         <div className="main">
             <Nav/>
             <Logo type={type}/>
             <Search onSubmit={searchReservations}/>
             <Selector type={type} onSubmit={getVersions}/>
-            <Result/>
-            <Recommand/>
+            <Result result={usable}/>
+            <Recommand recommands={recommends}/>
             <footer/>
         </div>
     );
