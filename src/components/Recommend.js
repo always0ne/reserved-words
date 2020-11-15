746c45bbf467d1728d8518e3af9dc51021dc3fee
always0ne <si8363@naver.com>
2020-11-15 21:12:34 +0900

    fix: resolve compile error

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,8 +1,8 @@
-import React, {useEffect,useState} from 'react';
+import React from 'react';
 import './css/Recommend.css';
 
 
-const Recommend = ({recommands,searchString}) => {
+const Recommend = ({recommands, searchString}) => {
 
     return (
         <div>
